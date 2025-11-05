9c2441afc5d79d0babfcc6317df4ba68b1ac9775
sinnedhax-oss <sinned.hax@gmail.com>
2025-11-05 17:07:11 -0300

    fix: corrigido import do tooltip para @radix-ui/react-tooltip

--- client/src/components/ui/tooltip.tsx
+++ client/src/components/ui/tooltip.tsx
@@ -2,13 +2,10 @@
 
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-
 import { cn } from "@/lib/utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
-
 const Tooltip = TooltipPrimitive.Root
-
 const TooltipTrigger = TooltipPrimitive.Trigger
 
 const TooltipContent = React.forwardRef<
